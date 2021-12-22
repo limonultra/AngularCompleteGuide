36413e1ba1994a9e75cea559935a09bcb18882bf
limonultra <mayuri.furude@gmail.com>
2021-12-22 01:31:29 +0100

    introduction + a little of basics

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server', 
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{//hacemos export para que la clase pueda ser accedida desde fuera
+
+    
+
+}
